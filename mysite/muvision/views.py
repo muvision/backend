53b172c80f1758af74004fe9385a2deae98a624c
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2023-07-24 22:10:18 -0400

    Add latex function

--- mysite/muvision/views.py
+++ mysite/muvision/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from PIL import Image
-from . import classification
+from .functions import classification
 
 
 def index(request):
@@ -11,6 +11,14 @@ def index(request):
 
 
 def classify_single(request):
+    if request.method == "POST":
+        print(request.FILES['image'])
+        image_file = Image.open(request.FILES['image'])
+        res = classification.classify(image_file)
+        return HttpResponse(f"This character is probably {res}")
+    return HttpResponse(f"Hello, world. Youre at the classification page. Please make a post request with the image in the body")
+
+def classify_image(request):
     if request.method == "POST":
         print(request.FILES['image'])
         image_file = Image.open(request.FILES['image'])
